9e1c92d386eccc304172e0d46216b6a35d83f3a6
Jonghun\JH <dubious123@hanyang.ac.kr>
2022-08-07 21:16:16 +0900

    git feat : add character rotation

--- Assets/02.Scripts/CharacterController.cs
+++ Assets/02.Scripts/CharacterController.cs
@@ -8,12 +8,16 @@ public class CharacterController : MonoBehaviour
 	#region SerializeFields
 	[SerializeField] private float _smoothInputSpeed;
 	[SerializeField] private float _moveSpeed;
+	[SerializeField] private float _rotationSpeed;
 	#endregion
 
 	private PlayerInput _playerInput;
 	private InputAction _moveAction;
+	private Vector2 _moveActionValue;
 	private Vector2 _currentInputVector;
 	private Vector2 _smoothVelocity;
+	private Vector3 _targetDir;
+	private Quaternion _targetRotation;
 	private void Awake()
 	{
 		_playerInput = GetComponent<PlayerInput>();
@@ -21,8 +25,21 @@ public class CharacterController : MonoBehaviour
 	}
 	private void Update()
 	{
-		_currentInputVector = Vector2.SmoothDamp(_currentInputVector, _moveAction.ReadValue<Vector2>(), ref _smoothVelocity, _smoothInputSpeed);
-		transform.Translate(_moveSpeed * Time.deltaTime * new Vector3(_currentInputVector.x, 0, _currentInputVector.y));
-		Debug.Log(_moveAction.ReadValue<Vector2>());
+		_moveActionValue = _moveAction.ReadValue<Vector2>();
+		#region Move
+		_currentInputVector = Vector2.SmoothDamp(_currentInputVector, _moveActionValue, ref _smoothVelocity, _smoothInputSpeed);
+		_targetDir = new Vector3(_currentInputVector.x, 0, _currentInputVector.y);
+		transform.Translate(_moveSpeed * Time.deltaTime * _targetDir, Space.World);
+		#endregion
+		#region Rotate
+		if (_moveActionValue != Vector2.zero)
+		{
+			_targetRotation = Quaternion.LookRotation(_moveSpeed * Time.deltaTime * _targetDir, Vector3.up);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, Time.deltaTime * _rotationSpeed);
+		}
+		#endregion
+
+
+
 	}
 }
