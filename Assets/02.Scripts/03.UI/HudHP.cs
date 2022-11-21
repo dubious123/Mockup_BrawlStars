d6f8ac83b4c8edc40f7cf267926eea9ad252acef
Jonghun\JH <dubious123@hanyang.ac.kr>
2022-11-21 21:10:29 +0900

    add ui take hit effect

--- Assets/02.Scripts/03.UI/HudHP.cs
+++ Assets/02.Scripts/03.UI/HudHP.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using Coffee.UIExtensions;
+
 using MEC;
+
+using TMPro;
 
 using Unity.VectorGraphics;
 
@@ -13,9 +17,10 @@ public class HudHP : MonoBehaviour
 	[SerializeField] private CPlayer _player;
 	[SerializeField] private float _holdTime;
 	[SerializeField] private float _followTime;
+	[SerializeField] private float _expandRate = 1.1f;
 	[SerializeField] private TextWithShadow _healthText;
 	[SerializeField] private RectTransform _canvasRect;
-	[SerializeField] private Vector2 _offset;
+	[SerializeField] private Vector3 _offset;
 	[SerializeField] private Transform _targetTransform;
 
 
@@ -29,9 +34,15 @@ public class HudHP : MonoBehaviour
 	[SerializeField] private Sprite _SpriteFollowSelf;
 	[SerializeField] private Sprite _SpriteFollowBlue;
 	[SerializeField] private Sprite _SpriteFollowRed;
+	[SerializeField] private UIParticle _particleBar;
+	[SerializeField] private UIParticle _particleTextBody;
+	[SerializeField] private UIParticle _particleTextShadow;
+	[SerializeField] private TextMeshProUGUI _bodyMask;
+	[SerializeField] private TextMeshProUGUI _shadowMask;
 	#endregion
 	private int _beforeHp;
 	private int _currentHp;
+	private bool _isSelf;
 	private RectTransform _rect;
 	private CoroutineHandle _coHandle;
 
@@ -39,15 +50,18 @@ public class HudHP : MonoBehaviour
 	{
 		_rect = GetComponent<RectTransform>();
 		_beforeHp = _currentHp = _player.Hp;
-		_healthText.Text = _currentHp.ToString();
+		SetText();
 
 		if (_player.TeamId == User.TeamId)
 		{
 			transform.parent.GetComponent<Canvas>().sortingOrder = 1;
 			_fillImage.sprite = _SpriteFillSelf;
-			_followImage.sprite = _SpriteFillSelf;
+			_followImage.sprite = _SpriteFollowSelf;
+			_isSelf = true;
+			return;
 		}
-		else if (_player.NPlayer.Team == User.Team)
+
+		if (_player.NPlayer.Team == User.Team)
 		{
 			_fillImage.sprite = _SpriteFillBlue;
 			_followImage.sprite = _SpriteFollowBlue;
@@ -57,11 +71,13 @@ public class HudHP : MonoBehaviour
 			_fillImage.sprite = _SpriteFillRed;
 			_followImage.sprite = _SpriteFollowRed;
 		}
+
+		_isSelf = false;
 	}
 
 	private void LateUpdate()
 	{
-		_rect.anchoredPosition = Camera.main.WorldtoCanvasRectPos(_canvasRect.sizeDelta, _targetTransform.position) + _offset;
+		_rect.anchoredPosition = Camera.main.WorldtoCanvasRectPos(_canvasRect.sizeDelta, _targetTransform.position + _offset);
 		_currentHp = _player.Hp;
 		if (_beforeHp == _currentHp)
 		{
@@ -69,24 +85,27 @@ public class HudHP : MonoBehaviour
 		}
 
 		Timing.KillCoroutines(_coHandle);
-		_coHandle = Timing.RunCoroutine(Co_OnHpChange());
-		_healthText.Text = _currentHp.ToString();
+		_coHandle = _isSelf ? Timing.RunCoroutine(Co_OnHpChangeSelf()) : Timing.RunCoroutine(Co_OnHpChange());
+		SetText();
 		_beforeHp = _currentHp;
 	}
 
 	private IEnumerator<float> Co_OnHpChange()
 	{
+		PlayParticle();
 		float elapsed = 0f;
 		float targetValue = _currentHp / (float)_player.MaxHp;
 		Vector2 anchorMax = _fillRect.anchorMax;
 		while (elapsed < _holdTime)
 		{
-			anchorMax.x = Mathf.Lerp(anchorMax.x, targetValue, elapsed / _holdTime);
+			var ratio = elapsed / _holdTime;
+			anchorMax.x = Mathf.Lerp(anchorMax.x, targetValue, ratio);
 			_fillRect.anchorMax = anchorMax;
 			elapsed += Timing.DeltaTime;
 			yield return Timing.WaitForOneFrame;
 		}
 
+		anchorMax.x = targetValue;
 		elapsed = 0f;
 		anchorMax = _followRect.anchorMax;
 		while (elapsed < _followTime)
@@ -97,5 +116,53 @@ public class HudHP : MonoBehaviour
 			yield return Timing.WaitForOneFrame;
 		}
 		yield break;
+	}
+
+	private IEnumerator<float> Co_OnHpChangeSelf()
+	{
+		PlayParticle();
+		float elapsed = 0f;
+		float targetValue = _currentHp / (float)_player.MaxHp;
+		var targetScale = _rect.localScale;
+		var currentScale = targetScale * _expandRate;
+		_rect.localScale = currentScale;
+		Vector2 anchorMax = _fillRect.anchorMax;
+		while (elapsed < _holdTime)
+		{
+			var ratio = elapsed / _holdTime;
+			anchorMax.x = Mathf.Lerp(anchorMax.x, targetValue, ratio);
+			_rect.localScale = Vector3.Lerp(currentScale, targetScale, ratio);
+			_fillRect.anchorMax = anchorMax;
+			elapsed += Timing.DeltaTime;
+			yield return Timing.WaitForOneFrame;
+		}
+
+		anchorMax.x = targetValue;
+		_rect.localScale = targetScale;
+		elapsed = 0f;
+		anchorMax = _followRect.anchorMax;
+		while (elapsed < _followTime)
+		{
+			anchorMax.x = Mathf.Lerp(anchorMax.x, targetValue, elapsed / _followTime);
+			_followRect.anchorMax = anchorMax;
+			elapsed += Timing.DeltaTime;
+			yield return Timing.WaitForOneFrame;
+		}
+		yield break;
+	}
+
+	private void PlayParticle()
+	{
+		_particleBar.Play();
+		_particleTextBody.Play();
+		_particleTextShadow.Play();
+	}
+
+	private void SetText()
+	{
+		var str = _currentHp.ToString();
+		_healthText.Text = str;
+		_bodyMask.text = str;
+		_shadowMask.text = str;
 	}
 }
